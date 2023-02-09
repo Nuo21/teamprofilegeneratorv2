dbaca7833328782dae931cdcbf2468360189b00e
Nuo <codypham5@gmail.com>
2023-02-09 18:37:42 -0500

    Updated test for employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,10 @@
 const Employee = require("../lib/Employee");
 
-test("Able to create an employee", () => {
-  const x = new Employee("Cody");
+test("Able to create an employee and all methods return correctly", () => {
+  const x = new Employee("Cody", 1, "cody@gmail.com");
+
+  expect(x.getName()).toBe("Cody");
+  expect(x.getId()).toBe(1);
+  expect(x.getEmail()).toBe("cody@gmail.com");
+  expect(x.getRole()).toBe("Employee");
 });
